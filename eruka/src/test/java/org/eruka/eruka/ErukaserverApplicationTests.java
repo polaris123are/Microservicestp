77c66438b36c8ae4772f17b473c456bbb9e4f1b9
polaris123are <polarisgoodman@gmail.com>
2023-12-10 21:55:56 +0100

    eureka-server

---
+++ eruka/src/test/java/org/eruka/eruka/ErukaserverApplicationTests.java
@@ -0,0 +1,13 @@
+package org.eruka.eruka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ErukaserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
